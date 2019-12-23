c67acbf3f4342aceee7b107e13e9b64c869c0a64
no8ody <no8ody@tanglebay.com>
2019-12-23 19:45:30 +0100

    update files

---
+++ config/config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+####################################################################
+# CONFIG FOR HORNET INSTALLER
+####################################################################
+
+os=ARM      # ARM = Raspberry PI3+/4 | x86_64 = VPS
+user=iota   # You can specify a own username for the hornet node
+
+####################################################################
+# CONFIG FOR TANGLEBAY SWARM
+####################################################################
+
+domain=my.domain.tld    #e.g. iotanode.noip.com
+
+####################################################################
+# CONFIG FOR TANGLEBAY SWARM
+####################################################################
+
+name="my node name" 
+port=14266
+pow=true    # Set if your node should do pow 'true' or 'false'
+password=   # If you had no password before, please let it empty
--- install_hornet.sh
+++ install_hornet.sh
@@ -5,7 +5,7 @@
 # DO NOT EDIT THE LINES BELOW !!!
 ############################################################################################################################################################
 ############################################################################################################################################################
-source config.sh
+source config/config.sh
 version="$(curl -s https://api.github.com/repos/gohornet/hornet/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
 version="${version:1}"
 
@@ -14,6 +14,7 @@ TEXT_YELLOW='\e[0;33m'
 TEXT_RED_B='\e[1;31m'
 
 echo -e $TEXT_YELLOW && echo "Starting installation of hornet" && echo -e $TEXT_RESET
+sudo apt install wget tar nano curl jq -y > /dev/null
 sudo useradd -m $user
 sudo -u $user mkdir /home/$user/hornet
 
@@ -23,7 +24,7 @@ sudo tar -xzf /tmp/HORNET-"$version"_Linux_"$os".tar.gz -C /tmp  > /dev/null
 sudo mv /tmp/HORNET-"$version"_Linux_"$os"/* /home/$user/hornet/  > /dev/null
 sudo rm -r /tmp/HORNET-"$version"_Linux_"$os"*  > /dev/null
 sudo wget -O /home/$user/hornet/latest-export.gz.bin https://dbfiles.iota.org/mainnet/hornet/latest-export.gz.bin  > /dev/null
-sudo cp ./config/config.json /home/$user/hornet/config.json
+sudo cp ./config/hornet.conf /home/$user/hornet/config.json
 sudo -u $user mkdir /home/$user/hornet/mainnetdb  > /dev/null
 sudo chown -R $user:$user /home/$user/hornet  > /dev/null
 sudo chmod 770 /home/$user/hornet/hornet  > /dev/null
@@ -56,4 +57,26 @@ echo -e $TEXT_YELLOW && echo "Starting hornet node! (please note this can may ta
 sudo systemctl start hornet
 echo -e $TEXT_YELLOW && echo "Loading live log and finish installation..." && echo -e $TEXT_RESET
 sudo journalctl -fu hornet
-echo -e $TEXT_RED_B && echo "Script finished... Bye!" && echo -e $TEXT_RESET
+echo -e $TEXT_RED_B && echo "Finish up hornet installation..." && echo -e $TEXT_RESET
+
+proxy=n
+read -p "You want set up a reverse proxy? (y/N): " proxy
+if [ $proxy == y ]
+    echo -e $TEXT_YELLOW && echo "Setting up reverse proxy..." && echo -e $TEXT_RESET
+    sudo add-apt-repository ppa:certbot/certbot
+    sudo apt install python-certbot-nginx -You
+    sudo apt update > /dev/null && sudo apt dist-upgrade -y > /dev/null && sudo apt upgrade -y > /dev/null && sudo apt autoremove -y > /dev/null
+    sudo cat config/nginx.conf > /etc/nginx/site-available/default
+    sudo systemctl restart nginx
+    sudo certbot --nginx -d $domain
+    echo -e $TEXT_YELLOW && echo "Finish reverse proxy setup..." && echo -e $TEXT_RESET
+fi
+
+
+read -p "You want to add your node to Tangle Bay? (y/N): " tbadd
+tbadd=n
+if [ $tbadd == y ]
+    echo -e $TEXT_YELLOW && echo "Adding node to Tangle Bay..." && echo -e $TEXT_RESET
+    curl https://community.tanglebay.org/nodes -X POST -H 'Content-type: application/json' -d '{"name": $name, "url": "https://$domain:$port", "pow": $pow}' |jq
+fi
+exit 0
