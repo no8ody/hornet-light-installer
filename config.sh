4ee804e1197da5c8623f547c24478baf7665fb78
TangleBay <59160642+TangleBay@users.noreply.github.com>
2019-12-23 11:00:57 +0100

    Add variables for hornet installer

--- config.sh
+++ config.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
+
+####################################################################
+# CONFIG FOR HORNET INSTALLER
+####################################################################
+
 # Please configure your settings below
 os=ARM
 version=0.2.9
 
 # You can specify a own username for the hornet node
 user=iota
+
+####################################################################
+# CONFIG FOR TANGLEBAY SWARM
+####################################################################
+
+# Please configure your settings
+name="my node name"
+domain=my.domain.tld
+port=443
+
+# Set if your node should do pow
+pow=true
+
+# You use already a reverse proxy?
+proxy=false
+
+# If you had no password before, please let it empty
+password=
